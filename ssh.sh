64a2ff8655b71e7f9dd63fa2e4c66de7ae68cc90
Vincenzo Maffione <v.maffione@gmail.com>
2016-05-18 12:20:09 +0200

    automatize ssh

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh -p 20090 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost
+ssh -p 20090 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile=irati_rsa root@localhost
