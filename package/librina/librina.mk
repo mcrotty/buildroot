25ba886033e73e717398b91e693e7d926734ef8c
Vincenzo Maffione <v.maffione@gmail.com>
2016-05-18 15:40:27 +0200

    prepare librina package

---
+++ package/librina/librina.mk
@@ -0,0 +1,17 @@
+############################################
+# librina				   #
+############################################
+
+LIBRINA_VERSION = master
+LIBRINA_SITE = https://github.com/IRATI/stack.git
+LIBRINA_SITE_METHOD = git
+LIBRINA_INSTALL_STAGING = yes
+LIBRINA_INSTALL_TARGET = yes
+LIBRINA_DEPENDENCIES = libnl host-pkgconf
+LIBRINA_LICENSE = LGPL
+LIBRINA_SUBDIR = librina
+LIBRINA_AUTORECONF = YES
+LIBRINA_AUTORECONF_OPTS = -i -f -Wall
+LIBRINA_CONF_OPTS = --disable-java-bindings --enable-debug --with-sysheaders=/home/vmaffione/git/nxw/irati/linux/usr/include
+
+$(eval $(autotools-package))
