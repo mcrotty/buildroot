8e5f3655e93c585b0a8a824ab00beec2d071c7df
Vincenzo Maffione <v.maffione@gmail.com>
2016-05-18 10:26:43 +0200

    qemu.sh: use local paths

--- qemu.sh
+++ qemu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qrun -o stdio -m 90 --kernel /home/vmaffione/git/nxw/irati/linux/arch/x86_64/boot/bzImage --initramfs /home/vmaffione/git/nxw/buildroot/output/images/rootfs.cpio
+qrun -o stdio -m 90 --kernel irati-bzImage --initramfs output/images/rootfs.cpio
