dd13f371e94b552aad9100e9b54a2c74c45d1561
Vincenzo Maffione <v.maffione@gmail.com>
2016-05-23 09:27:19 +0200

    make rina-tools package point to fakelinuxdep sources

--- package/rina-tools/rina-tools.mk
+++ package/rina-tools/rina-tools.mk
@@ -3,13 +3,18 @@
 ############################################
 
 RINA_TOOLS_VERSION = kernel-update
-RINA_TOOLS_SITE = https://github.com/IRATI/stack.git
-RINA_TOOLS_SITE_METHOD = git
+
+#RINA_TOOLS_SITE = https://github.com/IRATI/stack.git
+#RINA_TOOLS_SITE_METHOD = git
+#RINA_TOOLS_SUBDIR = rina-tools
+
+RINA_TOOLS_SITE = output/build/fakelinuxdep-kernel-update/rina-tools
+RINA_TOOLS_SITE_METHOD = local
+
 RINA_TOOLS_INSTALL_STAGING = YES
 RINA_TOOLS_INSTALL_TARGET = YES
 RINA_TOOLS_DEPENDENCIES = host-pkgconf librina rinad
 RINA_TOOLS_LICENSE = LGPL
-RINA_TOOLS_SUBDIR = rina-tools
 RINA_TOOLS_AUTORECONF = YES
 RINA_TOOLS_AUTORECONF_OPTS = -i -f -Wall
 RINA_TOOLS_CONF_ENV = PKG_CONFIG_PATH=/usr/lib/pkgconfig
