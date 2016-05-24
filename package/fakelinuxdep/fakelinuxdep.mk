92bff3c24f07e906bfb3ee8df7a07ccc950d9151
Vincenzo Maffione <v.maffione@gmail.com>
2016-05-24 16:32:23 +0200

    add fakelinuxdep hint for local download

--- package/fakelinuxdep/fakelinuxdep.mk
+++ package/fakelinuxdep/fakelinuxdep.mk
@@ -5,7 +5,12 @@
 # Use a version which includes 4.1.24 kernel, since it
 # can be compiled with gcc6
 FAKELINUXDEP_VERSION = kernel-update
+
 FAKELINUXDEP_SITE = https://github.com/IRATI/stack.git
+
+# Enable this one to fast download from a local repo
+#FAKELINUXDEP_SITE = file:///home/vmaffione/git/nxw/irati
+
 FAKELINUXDEP_SITE_METHOD = git
 FAKELINUXDEP_INSTALL_STAGING = YES
 FAKELINUXDEP_INSTALL_TARGET = YES
