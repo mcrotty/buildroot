64713602e1ed0f8212a7057d26d520d91997dc46
Vincenzo Maffione <v.maffione@gmail.com>
2016-07-13 11:52:27 +0200

    rinad-plugins: workaround kernel module install problem

--- package/rinad-plugins/rinad-plugins.mk
+++ package/rinad-plugins/rinad-plugins.mk
@@ -19,7 +19,8 @@ RINAD_PLUGINS_MODULE_SUBDIRS = plugins/cherish-urgency plugins/cong_avoidance pl
 
 RINAD_PLUGINS_DIRNAMES = cherish-urgency cong_avoidance lfa multipath red
 
-define RINAD_PLUGINS_INSTALL_TARGET_CMDS
+# This used to be RINAD_PLUGINS_INSTALL_TARGET_CMDS, see the explanation below
+define RINAD_PLUGINS_INSTALL_MANIFESTS
 	for plu in $(RINAD_PLUGINS_DIRNAMES) ; do \
 		manif=`ls -1 $(@D)/plugins/$${plu}/ | grep manifest` ; \
 		$(INSTALL) -D -m 0644 $(@D)/plugins/$${plu}/$${manif} $(TARGET_DIR)/lib/modules/4.1.24-irati/extra ; \
@@ -27,4 +28,14 @@ define RINAD_PLUGINS_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(kernel-module))
+
+# We have to make sure that buildroot installs the manifest files after the
+# "Install kernel module(s)" phase, because for some silly reason the latter
+# phase fails if the "extra" directory is already there - and the "extra"
+# directory is created by the manifests installation code, if needed.
+# That's why we have to use a post-install hook, and the hook must
+# be added here, after calling $(eval $(kernel-module)), which will append
+# the post-install hook for kernel modules installation.
+RINAD_PLUGINS_POST_INSTALL_TARGET_HOOKS += RINAD_PLUGINS_INSTALL_MANIFESTS
+
 $(eval $(generic-package))
